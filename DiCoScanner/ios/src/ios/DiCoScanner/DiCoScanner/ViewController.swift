72fb680ab76a9a64baec961cee3a94ba6c9437f8
Fabian Köbel <fabian.koebel@letsdev.de>
2020-03-20 21:43:25 +0100

    recreated project.

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiCoScanner
+//
+//  Created by Fabian Köbel on 20.03.20.
+//  Copyright © 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
