6884a8ef20ebea95d5687f807e62960aa8c1b562
Fabian Köbel <fabian.koebel@letsdev.de>
2020-03-21 19:10:18 +0100

    added app info controller to tabbar.

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/src/ui/controller/AppInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AppInfoViewController.swift
+//  DiCoScanner
+//
+//  Created by Fabian Köbel on 21.03.20.
+//  Copyright © 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "App-Info"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
