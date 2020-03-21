1bfd7a4232df523e0c23a9d804fb23d1d7cd53f8
Arne Fischer <arne.fischer@letsdev.de>
2020-03-21 16:51:26 +0100

    Added daos.

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/src/data/dao/SymptomDiaryEntryDao.swift
@@ -0,0 +1,11 @@
+//
+//  DiCoScanner
+//
+//  Created by Arne Fischer on 20.03.20.
+//  Copyright © 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import Foundation
+
+class SymptomDiaryEntryDao: BaseDao<SymptomDiaryEntry> {
+}
