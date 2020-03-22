df4daadbd0b108dc64aaf3dd8296dc72260474b6
Sebastian Ruf <sebastian.ruf@letsdev.de>
2020-03-22 15:19:04 +0100

    Styling of symptom cells

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/src/util/UIImage+SymptomImage.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+SymptomImage.swift
+//  DiCoScanner
+//
+//  Created by Sebastian Ruf on 22.03.20.
+//  Copyright © 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    
+    static func image(symptomId: String?)-> UIImage? {
+        if let id = symptomId, let image = UIImage(named: "ic_symptom_\(id)") {
+            return image
+        } else {
+            return UIImage(named: "ic_symptom_default") // TODO
+        }
+    }
+    
+}
