5cff74184d300fa03293d5dcb73c38ddfa4956d0
Fabian Köbel <fabian.koebel@letsdev.de>
2020-03-22 13:01:04 +0100

    added notification names.

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/src/util/NotificationNames.swift
@@ -0,0 +1,10 @@
+//
+// Created by Fabian Köbel on 22.03.20.
+// Copyright (c) 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let didSyncSymptomDiaryEntry = Notification.Name("didSyncSymptomDiaryEntry")
+}
