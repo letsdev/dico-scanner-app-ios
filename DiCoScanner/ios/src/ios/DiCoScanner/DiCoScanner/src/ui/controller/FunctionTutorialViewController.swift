acbb5710f66a71a0c55faa83470aa120d5cddac2
Sebastian Ruf <sebastian.ruf@letsdev.de>
2020-03-22 13:18:18 +0100

    Added function tutorial vc

---
+++ DiCoScanner/ios/src/ios/DiCoScanner/DiCoScanner/src/ui/controller/FunctionTutorialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FunctionTutorialViewController.swift
+//  DiCoScanner
+//
+//  Created by Sebastian Ruf on 22.03.20.
+//  Copyright © 2020 let's dev GmbH & Co. KG. All rights reserved.
+//
+
+import UIKit
+
+class FunctionTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
